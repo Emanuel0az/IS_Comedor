a39a5f53644750b909a520a6372657afd7866226
Emanuel0az <168204486+Emanuel0az@users.noreply.github.com>
2024-09-25 14:14:59 -0600

    creacion de componentes registro y login

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -1,4 +1,4 @@
-import Routing from './routes/routing'
+import Routing from '../Routes/routing'
 
 const App = () => {
   return (
---
+++ reactApp/src/Pages/register.jsx
@@ -0,0 +1,14 @@
+import Register from '../components/registro/Register'
+
+function Registers() {
+  return (
+    <div>
+
+
+      <Register />
+      
+    </div>
+  )
+}
+
+export default Registers
