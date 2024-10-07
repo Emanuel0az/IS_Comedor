ce841b7546b3a8fc615044bbc73c511ff2cd56de
SainexDo <ventizgene@gmail.com>
2024-10-07 08:23:48 -0600

    Agregacion de lógica para cambiar el color de la pagina.

--- reactApp/src/components/ToggleSwitch/ToggleSwitch.jsx
+++ reactApp/src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,26 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../ToggleSwitch/ToggleSwitch.css'
 import ModeNightIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
 const ToggleSwitch = () => {
-    const [colorState, setColorState] = useState(false)
-    localStorage.setItem('colorState', colorState)
+  const [colorState, setColorState] = useState(() => {
+    // Inicializa el estado a partir de localStorage
+    return localStorage.getItem('colorState') === 'true'; // Convierte a booleano
+  });
+  
+  const handleColorState = () => {
+    const newColorState = !colorState; // Calcula el nuevo estado
+    setColorState(newColorState);
+    localStorage.setItem('colorState', newColorState); // Guarda el nuevo estado
+  };
 
-    const handleColorState = () => {
-        setColorState(!colorState)
-    }
+  useEffect(() => {
+    // Actualiza el localStorage cuando colorState cambie
+    localStorage.setItem('colorState', colorState);
+  }, [colorState]);
+
 
 
   return (
-    <div onClick={(() => handleColorState())} className={`toggle_switch_container_${colorState ? 'day' : 'night'}`}>
-        <div className={`circleSwitch${colorState ? 'Day' : 'Night'}`} sry>
-            {colorState ? 
-            <div><ModeNightIcon style={{ fontSize: 15 }} className='imgNight'/></div> :
-            <div><LightModeIcon style={{ fontSize: 15 }} className='imgDay' /></div>
-            }
-        </div>
+    <>
+      {/*He creado esto para aplicar un displayNone, ya que si no,
+      los codigos de color se muestran como texto. */}
+     <div className='oculto'>
+      {document.body.style.backgroundColor = colorState ? '#ffffff' : '#262428'}
+      {document.body.style.color = colorState ? 'black' : 'white'}
     </div>
+
+      <div onClick={(() => handleColorState())} className={`toggle_switch_container_${colorState ? 'day' : 'night'}`}>
+          <div className={`circleSwitch${colorState ? 'Day' : 'Night'}`} >
+              {colorState ? 
+              <div><ModeNightIcon style={{ fontSize: 15 }} className='imgNight'/></div> :
+              <div><LightModeIcon style={{ fontSize: 15 }} className='imgDay' /></div>
+              }
+          </div>
+      </div>
+    </>
   )
 }
 
