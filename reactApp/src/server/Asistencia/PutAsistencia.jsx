1a190df0e527776d605061ed28d21bce67c27540
SainexDo <ventizgene@gmail.com>
2024-10-19 18:46:10 -0600

    Actualizacion del apartado de asistencias de acuerdo a la fecha del LocalStorage.

---
+++ reactApp/src/server/Asistencia/PutAsistencia.jsx
@@ -0,0 +1,28 @@
+const url = 'http://localhost:8000/api/hist_pagos/'
+
+const putAsistencia = async (id, updatedToy) => {
+  try {
+    const response = await fetch(url + id + '/', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedToy),  // Enviar los datos actualizados
+    });
+
+    if (!response.ok) {
+      throw new Error('Error en la solicitud PUT');
+    }
+
+    const data = await response.json();
+    console.log(data);  // Datos actualizados
+  } catch (error) {
+    console.error('ERROR PUT:', error);
+  }
+};
+
+
+export default putAsistencia;
+
+// Ejemplo de uso
+// updateToy(1, { name: 'New Toy Name', description: 'Updated description' });
